b18541166086a8b96efede8025dccd21ea13ab01
safin777 <sayedislamsafin7770555@gmail.com>
2022-01-26 23:28:06 +0600

    Promise callling technique

--- app.js
+++ app.js
@@ -9,10 +9,19 @@ console.log("task 1");
 const promise = new Promise(function(resolve , reject) {
   setTimeout(function(){
     if (status){
-      resolve();
+      resolve("succes");
     }else{
-      reject();
+      reject("failed");
     }
   },3000);
 });
+
+//calling the promised statement like this section 
+promise
+   .then(function(vlaue){
+     console.log(vlaue);
+   })
+   .catch(function(err){
+     console.log(err);
+   })
 console.log("task 3");
