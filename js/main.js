3be5ad7d67d54ba9d93ec38746157cce0ec49b95
safin777 <sayedislamsafin7770555@gmail.com>
2022-01-30 02:40:01 +0600

    Milestone wise module collected and design change by DOM operation

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 const mileStonesData = JSON.parse(data).data;
 
 //this function for laoding  all mile stone data as individual object from the json data
-function loadMileStoneData(){
+function loadMileStoneData() {
     //selecting the div where i want load each mile stone using js map() function
-    const mileStones = document.querySelector('.milestones');
+    const mileStones = document.querySelector(".milestones");
 
-    mileStones.innerHTML = `${mileStonesData.map(function(milestone){
-      return `<div class="milestone border-b">
-      <div class="flex">
+    mileStones.innerHTML = `${mileStonesData
+        .map(function (milestone) {
+            return `<div class="milestone border-b">
+      <div class="flex" >
         <div class="checkbox"><input type="checkbox" /></div>
-        <div>
+        <div onclick="openMileStone(this)">
           <p>
             ${milestone.name}
             <span><i class="fas fa-chevron-down"></i></span>
@@ -17,14 +18,34 @@ function loadMileStoneData(){
         </div>
       </div>
       <div class="hidden_panel">
-       ${milestone.modules.map(function(module){
-            return `<div class="module border-b">
+       ${milestone.modules
+           .map(function (module) {
+               return `<div class="module border-b">
             <p>${module.name}</p>
           </div>`;
-       }).join("")}
+           })
+           .join("")}
       </div>
     </div>`;
-    } ).join("")}`
+        })
+        .join("")}`;
+}
+
+function openMileStone(mileStoneElement) {
+    const currentPanel = mileStoneElement.parentNode.nextElementSibling;
+    const shownPanel = document.querySelector(".show");
+    const active = document.querySelector(".active");
+
+    if (active && !mileStoneElement.classList.contains("active")) {
+        active.classList.remove("active");
+    }
+    mileStoneElement.classList.toggle("active");
+
+    if (!currentPanel.classList.contains("show") && shownPanel) {
+        shownPanel.classList.remove("show");
+    }
+
+    currentPanel.classList.toggle("show");
 }
 
 loadMileStoneData();
