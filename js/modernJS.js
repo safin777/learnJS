1511155f2971fd08389c422cbae54e70b68a44d0
safin777 <sayedislamsafin7770555@gmail.com>
2022-01-31 23:52:44 +0600

    moder js array find started

--- js/modernJS.js
+++ js/modernJS.js
@@ -1,3 +1,4 @@
+// ###############  topic1:Fat arrow function  ##################
 
 //example of fat arrow function 
 //arrow function dont care about this keyword
@@ -9,7 +10,7 @@ const js = {
   printLib:function(){
       // #2 
       //let self = this.name;
-      
+
        this.libreries.forEach((lib)=>{
        console.log(`${this.name} loves ${lib}`);
     });
@@ -17,3 +18,47 @@ const js = {
 }
 
 js.printLib();
+
+
+//##########  topic2: truthy and falsy #################
+
+
+//false,0,NaN,undefine,"",null all of this are like falsy value
+
+var myvar =""; //change here and see what is the output
+
+if (myvar){  //here this myvar is checking variable which is checking the value is false or true
+  console.log("this is true");
+
+}else{
+  console.log("this is falsy");
+}
+
+
+//##############  topic3:ternary operator  ##################
+
+var age = 19;
+var ternary = (age>=18) ? "adult" : "under age"; //this is the ternary operator
+console.log(ternary);
+
+//if this is true or false in ternary condition you have just write the variable ..it will check the true or false value 
+var value = false;
+
+var access = value;
+console.log(access);
+
+//############# topic4 : Array.find() method ###############
+
+//lets create an array 
+
+let number  = [1,2,3,4,5,6,7,8,9,10];
+
+//if the function inside find function return true 
+//the searching will be stop there 
+//this mainly used for finding a particuler element from an array
+
+number.find(function(){
+   
+});
+
+
